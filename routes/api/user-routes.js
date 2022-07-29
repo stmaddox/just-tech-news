2fcd06edf9e780e81dd3624ef37e998ef0b034b6
Sean M <sean.maddox89@yahoo.com>
2022-07-29 03:50:35 -0700

    finished 13.1

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,17 +6,20 @@ const { User } = require('../../models');
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll()
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+        attributes: { exclude: ['password'] }
+    
+})
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 
 
 // GET api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
